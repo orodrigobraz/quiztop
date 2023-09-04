0848a200495961ef49474093c781670f2b422a04
Rodrigo Braz <orodrigobraz18@gmail.com>
2023-09-04 17:27:27 -0300

    Quantidade de perguntas

--- quiz.js
+++ quiz.js
@@ -22,7 +22,7 @@ async function carregarPerguntas() {
         const resposta = await fetch("perguntas.json");
         perguntas = await resposta.json();
 
-        const indicesSelecionados = selecionarIndicesAleatorios(perguntas.length, 302);
+        const indicesSelecionados = selecionarIndicesAleatorios(perguntas.length, 30);
         perguntas = indicesSelecionados.map(index => perguntas[index]);
 
         embaralharPerguntas();
